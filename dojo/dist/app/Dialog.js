587cc13411252636c85e44da98800470ede91979
sfrost <sfrost@scottlogic.com>
2013-10-28 14:52:15 +0000

    Corrected an issue where temporary files were put under source control source

--- dojo/dist/app/Dialog.js
+++
@@ -1,3 +0,0 @@
-//>>built
-define("app/Dialog",[],1);
-//@ sourceMappingURL=Dialog.js.map
--- dojo/release/dist/app/main.js
+++
@@ -1,3 +0,0 @@
-//>>built
-define(["dojo/has","require"],function(b,a){a(["dojo/parser","dojox/mobile","dojo/domReady!"],function(a,b){a.parse()})});
-//@ sourceMappingURL=main.js.map
